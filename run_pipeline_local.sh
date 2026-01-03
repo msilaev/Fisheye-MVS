5dc5af2d8320119d08da35261f494e6582904a83
Mikhail Silaev <mikesilaev@gmail.com>
2026-01-03 11:10:52 +0200

    pipeline

--- run_pipeline_local.sh
+++ run_pipeline_local.sh
@@ -21,26 +21,21 @@ EXPERIMENT_NAME="KITTI-360"
 IMAGE_DIR="IMAGES_DIR_experiment_1"
 
 REMOTE_LOG_DIR="${REMOTE_DIR_ROOT}/${EXPERIMENT_NAME}/log_metrics"
+remote_transform_result_path="${REMOTE_DIR_ROOT}/${EXPERIMENT_NAME}/transform_result.json"
+REMOTE_RESULTS_DIR="${REMOTE_DIR_ROOT}/${EXPERIMENT_NAME}/results"
 
 REMOTE_IMAGE_DIR="${REMOTE_DIR_ROOT}/${EXPERIMENT_NAME}/${IMAGE_DIR}"
-
-remote_transform_result_path="${REMOTE_DIR_ROOT}/${EXPERIMENT_NAME}/transform_result.json"
-
-REMOTE_RESULTS_DIR="${REMOTE_DIR_ROOT}/${EXPERIMENT_NAME}/results"
 
 
 REMOTE_SCRIPT_DIR="${REMOTE_DIR_ROOT}/src_remote"
 REMOTE_SCRIPT_DIR_unik3d="${REMOTE_DIR_ROOT}/src_remote"
 REMOTE_SCRIPT_DIR_superglue="${REMOTE_DIR_ROOT}/src_remote"
 REMOTE_SCRIPT_DIR_procrustes="${REMOTE_DIR_ROOT}/src_remote"
-
 REMOTE_SUPER_GLUE_DIR="/home/hdd/mikhail/GAUSSIAN-SPLATTING/SuperGluePretrainedNetwork"
 REMOTE_UNIK3D_DIR="/home/hdd/mikhail/GAUSSIAN-SPLATTING/MVF-UniK3D"
 
 
 LOCAL_ROOT="/worktmp/THESES/GAUSSIAN-SPLATTING/"
-
-
 LOCAL_SCRIPT_DIR="${LOCAL_ROOT}/Fisheye-MVS/"
 LOCAL_SCRIPT_DIR_unik3d="${LOCAL_ROOT}/Fisheye-MVS/src_unik3d/"
 LOCAL_SCRIPT_DIR_superglue="${LOCAL_ROOT}/Fisheye-MVS/src_superglue/"
@@ -69,6 +64,9 @@ rsync -avz "$LOCAL_SCRIPT_DIR_procrustes/" \
 
 rsync -avz "$LOCAL_SCRIPT_DIR/run_pipeline_remote.sh" \
     "$REMOTE_USER@$REMOTE_HOST:$REMOTE_SCRIPT_DIR/run_pipeline_remote.sh"
+
+rsync -avz "$LOCAL_IMAGE_DIR/" \
+    "$REMOTE_USER@$REMOTE_HOST:$REMOTE_IMAGE_DIR/"    
 
 ssh "$REMOTE_USER@$REMOTE_HOST" \
   "nohup bash '$REMOTE_SCRIPT_DIR/run_pipeline_remote.sh' \
